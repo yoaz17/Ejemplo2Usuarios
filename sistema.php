fbadfc2194a4fc7be169a0f1582da0d12cdeafc3
yoaz17 <43894711+yoaz17@users.noreply.github.com>
2018-10-09 08:56:19 -0500

    Update sistema.php

--- sistema.php
+++ sistema.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+require_once 'LIGA3/LIGA.php';
+ BD('localhost', 'root', ' ', 'sistemascd');
+ $tabla = 'usuarios';
+ $liga = LIGA($tabla);
+
 if(empty($_SESSION['id']) && !empty($_SESSION['Pass'])){
     //echo 'Usuario OK';
     require_once 'LIGA3/LIGA.php';
@@ -8,6 +13,15 @@ if(empty($_SESSION['id']) && !empty($_SESSION['Pass'])){
     HTML::cabeceras(array('title' => 'Sistema Seguro', 'description' => 'Lo que sea...'));
     
     $body = array('contenedor' => array('uno' => '<p>Usuario Válido</p>', 'dos' => '<a href = "cerrar.php"> Cerrar Sesion</a> '));
+    
+    
+    $cols=array('*', '-Pass');
+    $join = array('depende' => $liga);
+    $pie = '<th colspan="@[numCols]">Total de instancias:@[numReg]</th>';
+    HTML::tabla($liga, 'Instancias de' .$tabla, $cols, true, $join, $pie);
+    
+    
+    
     HTML::cuerpo($body);
     HTML::pie();
     
